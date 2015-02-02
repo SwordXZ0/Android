5bc2985bf9506100465f1df8d737144693ed8d94
SwordXZ0 <winzero_8@hotmail.com>
2015-02-01 23:19:03 -0600

    Fragmento de LogIn con actividad principal creado y funcionando

---
+++ app/src/main/java/mx/itesm/acoustics/acoustics/MainActivity.java
@@ -0,0 +1,89 @@
+package mx.itesm.acoustics.acoustics;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ScrollView;
+
+
+public class MainActivity extends ActionBarActivity {
+    public static final ColorDrawable[] colorToRed = {new ColorDrawable(), new ColorDrawable(0xFFFF6F04)};
+    public static final ColorDrawable[] colorToBlue = {new ColorDrawable(Color.BLUE), new ColorDrawable(Color.RED)};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+        }
+
+        ScrollView screen;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            /*ScrollView frm = (ScrollView)getActivity().findViewById(R.id.intro);
+            TransitionDrawable trans = new TransitionDrawable(MainActivity.colorToRed);
+            frm.setBackgroundDrawable(trans);
+            trans.startTransition(5000);*/
+        }
+    }
+
+
+}
