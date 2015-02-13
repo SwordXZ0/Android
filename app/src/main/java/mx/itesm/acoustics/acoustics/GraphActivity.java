acd112e3ffd17a680e0da9cf18e82972f74c3581
SwordXZ0 <winzero_8@hotmail.com>
2015-02-13 12:50:47 -0600

    Drawerlayout agregado

--- app/src/main/java/mx/itesm/acoustics/acoustics/GraphActivity.java
+++ app/src/main/java/mx/itesm/acoustics/acoustics/GraphActivity.java
@@ -1,21 +1,108 @@
 package mx.itesm.acoustics.acoustics;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 public class GraphActivity extends ActionBarActivity {
+    private String[] itemsTitulos;
+    private DrawerLayout drawerLayout;
+    private ListView listViewDrawer;
+    private ActionBarDrawerToggle toggle;
+
+    private CharSequence tituloDrawer, titulo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.containerGraph, new GraphFragment())
                     .commit();
+        }*/
+        titulo=tituloDrawer=getTitle();
+        itemsTitulos=getResources().getStringArray(R.array.opciones);
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        listViewDrawer=(ListView)findViewById(R.id.left_drawer);
+
+        Objeto[] opciones= new Objeto[3];
+        opciones[0]=new Objeto(R.drawable.ic_action_graph, itemsTitulos[0]);
+        opciones[1]=new Objeto(R.drawable.ic_action_data, itemsTitulos[1]);
+        opciones[2]=new Objeto(R.drawable.ic_action_return, itemsTitulos[2]);
+
+        DrawerAdaptador adaptador= new DrawerAdaptador(this,R.layout.renglon,opciones);
+        listViewDrawer.setAdapter(adaptador);
+
+        listViewDrawer.setOnItemClickListener(new DrawerListListener());
+
+        if (savedInstanceState == null) {
+            seleccionaAccion(0);
+        }
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.abrir,R.string.cerrar){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(tituloDrawer);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(titulo);
+            }
+        };
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+        drawerLayout.setDrawerListener(toggle);
+    }
+
+    public class DrawerListListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            seleccionaAccion(position);
+        }
+    }
+
+    public void seleccionaAccion(int p){
+        Fragment fragment=null;
+        switch (p){
+            case 0:
+                fragment=new GraphFragment();
+                break;
+            case 1:
+                fragment=new DetailDataFragment();
+                break;
+            case 2:
+                //fragment=new GraphFragment();
+                finish();
+                break;
+        }
+        if(fragment!=null){
+            getFragmentManager().beginTransaction().replace(R.id.content_frame,fragment).commit();
+            listViewDrawer.setItemChecked(p,true);
+            listViewDrawer.setSelection(p);
+            setTitle(itemsTitulos[p]);
+            drawerLayout.closeDrawer(listViewDrawer);
+        }else{
+            listViewDrawer.setItemChecked(p,true);
+            listViewDrawer.setSelection(p);
+            setTitle(itemsTitulos[p]);
+            drawerLayout.closeDrawer(listViewDrawer);
         }
     }
 
@@ -32,10 +119,10 @@ public class GraphActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true;
         }
 
---
+++ app/src/main/java/mx/itesm/acoustics/acoustics/Objeto.java
@@ -0,0 +1,14 @@
+package mx.itesm.acoustics.acoustics;
+
+/**
+ * Created by sky on 12/02/2015.
+ */
+public class Objeto {
+    public int icono;
+    public String nombre;
+
+    public Objeto(int i, String n){
+        icono=i;
+        nombre=n;
+    }
+}
